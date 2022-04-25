e6aa105625a0747e9d218c2513572706c8222763
sjsbee <93555209+sjsbee@users.noreply.github.com>
2022-04-25 05:11:46 -0500

    first commit

---
+++ practice files.js
@@ -0,0 +1,9 @@
+function FirstReverse(str) { 
+    return str.split('').reverse().join('');
+    
+    }
+   
+    //Remember array methods require parentheses and using '' conveys every character.
+    //split separates each character
+    //reverse reverses the order of the array
+    //join reconnects the array
